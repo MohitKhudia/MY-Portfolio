9c9d60114b628bb08b9e0d7346baed1db26b6b40
Mohit <mohitkhudia7@gmail.com>
2025-04-26 17:45:46 +0530

    Dashboard Added

--- src/app/Projects.jsx
+++ src/app/Projects.jsx
@@ -1,9 +1,17 @@
 import OwnPortfolio from "../assets/img/OwnPortfolio.png"
+import Dashboard from "../assets/img/Dashboardpng.png"
 export const Projects = [
         {
             img : OwnPortfolio ,
             title : "Portfolio" ,
             Description : "This Is My Own Portfolio ! Those You see Now" ,
-            Link : "http://localhost:3000/"
+            Link : "https://my-portfolio-three-eosin-52.vercel.app/Workshop"
+        },
+        {
+            img : Dashboard ,
+            title : "Dashboard" ,
+            Description : "Admin panel Dashboard we use firebase for Data Store and auth " ,
+            Link : "https://dashboard-cdv4vqpau-mohits-projects-5d223420.vercel.app/",
+            size : 'desktop only'
         }
 ]
--- src/app/Workshop/page.jsx
+++ src/app/Workshop/page.jsx
@@ -7,25 +7,28 @@ const Workshop = () => {
     <main>
       <section>
         <div className="container mx-auto max-w-[1240px] px-3 mt-4">
-          <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+          <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
             {Projects.map((value, index) => {
               return (
                 <div
-                  className="border p-3 rounded-2xl hover:-translate-y-1 transition-all duration-200 backdrop-blur-md bg-white/20"
+                  className="border p-3 relative rounded-2xl hover:-translate-y-1 transition-all duration-200 backdrop-blur-md bg-white/20"
                   key={index}
                   data-aos = "zoom-in"
                 >
                   <img src={value.img.src} alt="Project Img" />
+                  {value.size ? (<div className="bg-black px-2 py-1.5 text-sm absolute rounded-full -top-[15px] lg:-right-[30px] right-1/2 translate-x-1/2 lg:translate-x-0 ff-Bree text-white">
+                      {value.size}
+                  </div>) : ''}
                   <h2 className="text-xl lg:text-2xl ff-Bree mt-2.5">
-                    Title :- {value.title}
+                    Title :- {value.title} 
                   </h2>
-                  <h2 className="text-xl lg:text-2xl ff-Bree mt-2.5">
+                  <h2 className="text-[16px] lg:text-xl ff-Bree mt-2.5">
                     Description :- {value.Description}
                   </h2>
                   <Link
                     href={value.Link}
                     target="_blank"
-                    className="text-xl lg:text-2xl ff-Bree mt-2.5"
+                    className="text-[16px] lg:text-xl ff-Bree mt-2.5"
                   >
                     Link :-
                     <span className="hover:underline hover:text-blue-500 cursor-pointer">
